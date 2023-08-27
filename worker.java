1b9cac127d1d9680c54b4633a10951a186c690d2
Andrew Wesel <andrew.wesel@gmail.com>
2023-08-26 20:44:30 -0700

    first commit. includes all original and generated files. Main code in worker.java

---
+++ worker.java
@@ -0,0 +1,52 @@
+package file_read;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * worker
+ */
+public class worker {
+    public static void writeFile(String toWrite) {
+        String name = "write_this.txt";
+
+        // Write the string to the file
+        try {
+            FileWriter fileWriter = new FileWriter(name);
+            fileWriter.write(toWrite);
+            fileWriter.close();
+        } catch (IOException ex) {
+            System.out.println("some didn't work");
+        }
+    }
+
+    public static String readFile(File file) {
+        String currentString = "";
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            while (reader.ready()) {
+                char nextChar = (char) reader.read();
+                currentString += nextChar;
+
+            }
+            reader.close();
+        } catch (IOException ex) {
+            System.out.println("nah");
+        }
+        return currentString;
+
+    }
+
+    public static void main(String args[]) {
+        File file = new File("read_this.txt");
+        String text = readFile(file);
+        System.out.println(text);
+        writeFile(text);
+    }
+
+}
